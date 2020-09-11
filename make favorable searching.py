740699dfd8842dbb9a8be2c819910d6134c7f3b1
keep and carry on <51162782+amhamhamh@users.noreply.github.com>
2020-09-11 19:40:11 +0900

    jo coding practice files

---
+++ make favorable searching.py
@@ -0,0 +1,15 @@
+#!C:\Users\amham\AppData\Local\Programs\Python\Python38-32\python.exe
+#!python
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+
+response = urlopen('https://www.daum.net/')
+soup = BeautifulSoup(response, 'html.parser')
+i=0
+for anchor in soup.select("a.link_favorsch"):
+    i+=1
+    print(str(i)+'위'+anchor.get_text())
+
+   
+
+    
