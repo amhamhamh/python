c045c45d9bc3295fdafca93a37e1447c00b84ea9
myeongHeon <amhamh0501@gmail.com>
2024-03-15 15:53:57 +0900

    dict

--- test.py
+++ test.py
@@ -213,10 +213,193 @@ a = [[10, 20], [30, 40], [50, 60]]
     
 
 
-a = []
+# a = []
 
-for i in range(10):
-    a.append(0)
+# for i in range(10):
+#     a.append(0)
+
+# for i in range(3):
+#     temp = []
+#     # a.append(temp)
+#     for j in range(2):        
+#         temp.append(0)
+#     a.append(temp)
+
+# a = [[0 for j in range(2)] for i in range(3)]
+# a = [[0] * 2 for j in range(3)]
+# print(a)
+
+
+# a = [3, 1, 3, 2, 5]    # 가로 크기를 저장한 리스트
+# b = []    # 빈 리스트 생성
+
+# for j in a:
+#     temp = []
+#     for i in range(j):
+#         temp.append(0)
+#     b.append(temp)
     
-    
-print(a)
+# print(b)
+
+# a = [[0] * i for i in [3, 1, 3, 2, 5]]
+
+# print(a)
+
+# a = [[0] * i for i in [3, 1, 3, 2, 5]]
+
+# a = [[10, 20], [30, 40]]
+# import copy  
+
+# c = a.copy() #  주소 복사, 참조
+# b = copy.deepcopy(a) 
+# b[0][0] = 500
+
+
+# print(a)
+# print(b)
+# print(c)
+
+# b = copy.deepcopy(a) # 값복사
+
+
+# print(b)
+
+
+# [
+#     [
+#         [0, 0, 0], 
+#         [0, 0, 0], 
+#         [0, 0, 0], 
+#         [0, 0, 0]
+#     ], 
+#     [
+#         [0, 0, 0], 
+#         [0, 0, 0], 
+#         [0, 0, 0], 
+#         [0, 0, 0]
+#     ]
+
+# ]
+
+# k = [[0] * 3 for i in range(2) for j in range(4)]
+
+# k = [[[0 for col in range(3)] for row in range(4)] for depth in range(2)]
+
+# print(k)
+
+
+
+# s = 'Hello, world!'
+# s = s.replace('Hello', '헬로')
+# print(s)
+
+# table = str.maketrans('aeiou', '12345') #문자 바꾸기 maketrans
+# 'apple'.translate(table)
+
+
+# k =  'apple pear grape pineapple orange'
+# j = k.split() # 공백을 기준으로 분리하여 리스트로 변환
+
+# print(k)
+# print(j)
+
+
+# p = '-'.join(['apple', 'pear', 'grape', 'pineapple', 'orange'])
+# print(p)
+
+# k = '   Python  .  '.lstrip()
+# print(k)
+
+# p =', python.'.lstrip(',.')
+# print(p)
+
+
+# p = 'python'.ljust(10) # 왼쪽 정렬
+# p1 = 'python'.rjust(10) # 오른쪽 정렬
+# print(p1)
+
+
+# p1 = ' 35'.zfill(6) #왼쪽부터 빈칸은 0으로 채움
+# print(p1)
+
+# o =  'apple pineapple'.find('pl') #해당 문자의 인덱스
+
+
+# o =  'apple pineapple'.find('xy') # find에서 해당 문자열이 없으면 -1을 반환
+
+# print(o)
+
+## 어떤 문자를 찾는 것은 순서를 가져오는 것. find
+## count('pl') -  이것은 해당 문자와 같은 값을 가져오는 것. 
+
+# o = 'apple pineapple'.count('pl')
+# print(o)
+
+# o = 'I am %s' % 'keep'
+# o = 'I am %d' % 12
+
+# o = 'I am %f' % 2.1 # 기본적으로 소수점 6자리까지..
+# print(o)
+
+# o = 'I am %10s' % 'learner'  # 왼쪽부터 10칸을 비워놓고 오른쪽 정렬
+# print(o)
+
+# o = 'I am %10d' % 150
+# print(o)
+
+# o = '%-10s' % 'python' # 왼쪽부터 10칸 비워서
+# print(o)
+
+# o = 'Today is %d %s' % (3, 'm') # 왼쪽부터 10칸 비워서
+# print(o)
+
+
+# language = 'Python'
+# version = 3.6
+# print(f'{language} {version}')
+
+
+# k = '{0:>10}'.format('10칸') # 10칸 비우기
+
+# k = '%03d' % 1 # 숫자 갯수는 3개 
+## 0자리수.소수점자리f
+# k = '{0:03d}'.format(35)
+
+# k = '%08.2f' % 3.6
+# k = format(121987851943500, ',') # format(숫자, '구분자') 만약에 ','를 넣으면 자리수 3개로 나눌 수 있음. 
+# k = '{0:>20}'.format(193570) # format(숫자, '구분자') 만약에 ','를 넣으면 자리수 3개로 나눌 수 있음. 
+# path = 'C:\\Users\\dojang\\AppData\\Local\\Programs\\Python\\Python36-32\\python.exe'
+# k = path.split('\\')
+
+# print(k)
+# print(k[-1])
+
+
+# 리스트는 실제 데이터 보다 더 큰 메모리를 사용하는 반면, 튜플은 고정된 메모리를 사용
+# x = {'a': 10, 'b': 20, 'c': 30, 'd': 40}
+# x.update(a=50) ## 해당 데이터를 수정하는 방법
+# print(x)
+
+# y = {1: 'one', 2: 'two'}
+# y.update({1:'ONE', 2:'TWO'})
+# y.update([[2, 'TWO'], [4, 'FOUR']])
+# y.update([[2, 'TWO'], [3, 'THREE']])
+# y = {'a': 10, 'b': 20, 'c': 30, 'd': 40}
+# y.pop('a')
+# print(y)
+# del y['a']
+# print(y)
+
+# x = {'a': 10, 'b': 20, 'c': 30, 'd': 40}
+#p = x.popitem() # 임의로 삭제하는 키-값 쌍을 튜플로 반환. 
+# l = x.get('z', 0)
+# l = x.items() # 키-값 쌍을 모두 가져옴. 
+# c = x.keys() # 모든 키를 가져옴. 
+# o = x.values() # 모든 키를 가져옴. 
+# print(c, o)
+
+# keys = ['a', 'b', 'c', 'd']
+# # x = dict.fromkeys(keys)  # 해당 키 리스트를 딕셔너리로 생성하며, 모두 None 값으로 생성
+# y = dict.fromkeys(keys)
+# print(x)
+
